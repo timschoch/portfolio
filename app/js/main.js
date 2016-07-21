eabd0f0edb2de414ad89e1bc8b8e58c2e2dc4826
Tim Schoch <tim.schoch@ioz.ch>
2016-07-21 17:55:55 +0200

    javscript navigatino stuff

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,39 @@
 /* jshint devel:true */
 
 /* navigation */
-var $navigationLinks = $('#pageHeader nav a');
-$navigationLinks.click(function () {
-	$(this).parent().addClass( 'selected' )
-		.siblings().removeClass( 'selected' );
+var $navigationItems = $('#pageHeader nav li'),
+	$navigationLinks = $navigationItems.find('a'),
+	scrollSpeed = 2; // how many px per millisecond
+
+var activateLink = function ($link) {
+	// set classes
+	$navigationItems.removeClass('selected prev');
+	$link.parent().addClass('selected')
+		.prev().addClass('prev');
+}
+
+$navigationLinks.click(function (e) {
+	e.preventDefault();
+
+	// scroll
+	var target = $(this.hash).offset().top,
+		currentPosition = $('html').scrollTop(),
+		distance = Math.round(Math.abs(target - currentPosition)),
+		boost = Math.ceil(distance / 1000),
+		speed = distance / scrollSpeed / boost;
+
+	$('html, body').animate({
+          scrollTop: target
+	}, speed);
+
+    return false;
 });
 
 var $waypoint = $('section.waypoint').waypoint(function (direction) {
-	// $('#' + this.element.id).addClass('foo');
-	// alert(this.element.id + ' hit 25% from ' + direction + ' of window');
 	if ('up' == direction) {
-		$navigationLinks.filter('.' + this.element.id).parent().prev().children().click();
+		activateLink( $navigationLinks.filter('.' + this.element.id).parent().prev().children() );
 	} else {
-		$navigationLinks.filter('.' + this.element.id).click();
+		activateLink( $navigationLinks.filter('.' + this.element.id) );
 	}
 }, {
 	offset: '25%'
