bc5539b7c3800779a0ce9f70d3296f5dfceea24b
Tim Schoch <tim.schoch@ioz.ch>
2016-07-22 07:34:24 +0200

    start with s3 deployment

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var reload = browserSync.reload;
 
 var through2 = require('through2');
 var browserify = require('browserify');
+var awspublish = require('gulp-awspublish');
 
 var isDevelopment = (process.env.ENVIRONMENT !== "production");
 
@@ -165,6 +166,38 @@ gulp.task('wiredep', function () {
       ignorePath: /^(\.\.\/)*\.\./
     }))
     .pipe(gulp.dest('app'));
+});
+
+// publish to amazon s3
+gulp.task('publish', ['build'], function () {
+  // create a new publisher using S3 options
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+  var publisher = awspublish.create({
+    region: 'eu-central-1',
+    params: {
+      Bucket: 'timschoch.com'
+    }
+  });
+
+  // define custom headers
+  var headers = {
+    'Cache-Control': 'max-age=315360000, no-transform, public'
+    // ...
+  };
+
+  return gulp.src('./dist/**')
+     // gzip, Set Content-Encoding headers and add .gz extension
+    .pipe(awspublish.gzip({ ext: '.gz' }))
+
+    // publisher will add Content-Length, Content-Type and headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+    .pipe(publisher.publish(headers))
+
+    // create a cache file to speed up consecutive uploads
+    .pipe(publisher.cache())
+
+     // print upload updates to console
+    .pipe(awspublish.reporter());
 });
 
 gulp.task('build', ['html', 'images', 'fonts', 'extras'], function () {
